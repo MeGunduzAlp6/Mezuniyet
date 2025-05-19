bd28298b51e8a2a838e384cc4bf28c8d650e2345
MeGunduzAlp6 <gunduzalpilipinar@gmail.com>
2025-05-19 19:25:12 +0300

    Update web_program.py
    h

--- web_program.py
+++ web_program.py
@@ -8,5 +8,21 @@ app = Flask(__name__)
 def web_skin():
     return render_template("web_skin.html")
 
+@app.route('/iklim_degisikligi')
+def iklim_degisikligi():
+    return render_template("iklim_degisikligi.html")
+
+@app.route('/iklim_degisikligi2')
+def iklim_degisikligi2():
+    return render_template("iklim_degisikligi2.html")
+
+@app.route('/iklim_degisikligi3')
+def iklim_degisikligi3():
+    return render_template("iklim_degisikligi3.html")
+
+@app.route('/iklim_degisikligi4')
+def iklim_degisikligi4():
+    return render_template("iklim_degisikligi4.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
